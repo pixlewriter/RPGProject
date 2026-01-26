bc056e3f8ea1b9b79be764c55e6099c57226c12b
pixlewriter <ryan@kufeldt.com>
2026-01-26 16:39:13 -0500

    Added functions to player class

--- RPGProject/Player.cpp
+++ RPGProject/Player.cpp
@@ -1,3 +1,16 @@
 #include "Player.h"
 #include "Character.h"
 #include "Enemy.h"
+
+void Player::attack(Character& character) {
+	double dmg = getStrength();
+	character.damageDealt(dmg);
+}
+
+void Player::heal() {
+	heal();
+}
+
+void damageDelt(double damage) {
+	damageDelt(damage);
+}
